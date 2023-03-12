723284b3445b2d6fc23a1cd26ccb449c902a2005
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-03-12 15:55:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-// import Navbar from "./Components/Navbar"
+import Navbar from "./Components/Navbar"
 function App() {
   return (
    <>
-   <h1>vhvhvhj</h1>
-   {/* < Navbar /> */}
+   {/* <h1>vhvhvhj</h1> */}
+   < Navbar />
   </>
   );
 }
