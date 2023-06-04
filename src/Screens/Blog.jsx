104c97815a5251bf12b20db45abfc4294e6c99df
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-04 05:30:52 +0530

    commit

--- src/Screens/Blog.jsx
+++ src/Screens/Blog.jsx
@@ -6,7 +6,8 @@ import IconCard from "../Components/IconCard";
 import Aos from "aos";
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
-
+import BlogData from "../Components/BlogData";
+import BlogCard from "../Components/BlogCard"
 const Bolgs = () => {
     useEffect(()=>{
         Aos.init({duration: 1800});
@@ -15,13 +16,8 @@ const Bolgs = () => {
     <>
     <div id="skill-main">
     <div id="blog" className="my-4 mx-auto blog px-5">
-      {/* <Typed
-        className="typed d-flex justify-content-center font-weight-bold pb-2"
-        strings={["Blogs..."]}
-        typeSpeed={40}
-      /> */}
       <h1 className="typed d-flex justify-content-center font-weight-bold pb-2">BLOGS...</h1>
-      {/* <div className="row">
+      <div className="row">
         {BlogData.map((value, ind) => {
           return (
             <BlogCard
@@ -29,13 +25,12 @@ const Bolgs = () => {
               title={value.title}
               description={value.description}
               published={value.published}
-              read={value.read}
               imgsrc={value.imgsrc}
               link={value.link}
             ></BlogCard>
           );
         })}
-      </div> */}
+      </div>
       <div className="justify-content-center  order-lg-2 header-img pt-4">
                         <h2 >Contact me.</h2>
                       </div>
