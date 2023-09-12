53d036b494c8d0b636a4507fb935db179ba01e1a
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-09-12 19:16:47 +0530

    commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Resume from "../Images/BogeshwararaoResume.pdf"
+import Resume from "../Images/b5os.pdf"
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 import { BrowserRouter ,
   Routes,
